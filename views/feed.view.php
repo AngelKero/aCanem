02408a3bf62810926113c2d81f955be0cc9c445d
Angel Zaragoza <zangelweb@gmail.com>
2020-10-27 12:06:31 -0600

    Se agrego la navegacion para moviles

--- views/feed.view.php
+++ views/feed.view.php
@@ -40,7 +40,7 @@
             </form>
             <ul class="buttons-navigation">
                 <li class="button-home">
-                    <a href="index.html">
+                    <a href="index.php">
                         <p class="icon-button"><i class="fas fa-home"></i></p>
                         <p class="text-button">Inicio</p>
                     </a>
@@ -202,5 +202,34 @@
             </div>
         </section>
     </main>
+    <aside class="navegacion-movil">
+        <ul class="buttons-navigation">
+            <li class="button-home">
+                <a href="index.php">
+                    <p class="icon-button"><i class="fas fa-home"></i></p>
+                </a> 
+            </li>
+            <li class="button-cuenta">
+                <a href="#">
+                    <p class="icon-button"><i class="fas fa-user"></i></p>
+                </a>
+            </li>
+            <li class="button-messages">
+                <a href="#">
+                    <p class="icon-button"><i class="fas fa-inbox"></i></p>
+                </a>
+            </li>  
+            <li class="button-notifications">
+                <a href="#">
+                    <p class="icon-button"><i class="fas fa-bell"></i></p>
+                </a>
+            </li>
+            <li class="button-options">
+                <a href="#">
+                    <p class="icon-button"><i class="fas fa-bars"></i></p>
+                </a>
+            </li>
+        </ul>
+    </aside>
 </body>
 </html>
