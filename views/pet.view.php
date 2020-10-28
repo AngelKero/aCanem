88b9b9a82a82801040d226850d721224e37dc2c8
Angel Zaragoza <zangelweb@gmail.com>
2020-10-28 12:43:38 -0600

    Se agregaron estilos al formulario para adoptar

--- views/pet.view.php
+++ views/pet.view.php
@@ -24,10 +24,10 @@
                         <div class="reasons info-block">
                             <h5>Caracteristicas: <i class="fas fa-paw"></i></h5>
                             <p>
-                                <span style="font-weight: bolder;">Tipo</span> | Canino -- 
-                                <span style="font-weight: bolder;">Raza</span> | Meztiza -- 
-                                <span style="font-weight: bolder;">Peso</span> | 1.3kg -- 
-                                <span style="font-weight: bolder;">Tamaño</span> | Pequeño -- 
+                                <span style="font-weight: bolder;">Tipo</span> | Canino --
+                                <span style="font-weight: bolder;">Raza</span> | Meztiza --
+                                <span style="font-weight: bolder;">Peso</span> | 1.3kg --
+                                <span style="font-weight: bolder;">Tamaño</span> | Pequeño --
                                 <span style="font-weight: bolder;">Edad</span> | 4 años <br><br>
                                 <span style="font-weight: bolder;">Sociable con Perros</span> | Si <br><br>
                                 <span style="font-weight: bolder;">Sociable con Niños</span> | Si <br><br>
@@ -35,8 +35,8 @@
                                 <br>
                                 <span style="font-weight: bolder;">Energia</span> | Grande <br><br>
                                 <span style="font-weight: bolder;">Espacio Requerido</span> | Mediano <br> <br>
-                                <span style="font-weight: bolder;">Sexo</span> | Macho -- 
-                                <span style="font-weight: bolder;">Esterilizado</span> | No -- 
+                                <span style="font-weight: bolder;">Sexo</span> | Macho --
+                                <span style="font-weight: bolder;">Esterilizado</span> | No --
                                 <span style="font-weight: bolder;">Desparasitado</span> | Si <br><br>
                             </p>
                         </div>
@@ -53,7 +53,8 @@
                             <span style="font-weight: bolder;">Causa</span> | Ninguna <br><br>
                             <span style="font-weight: bolder;">Email</span> | don.mamas@outlook.com <br><br>
                             <span style="font-weight: bolder;">Telefono</span> | 3325094748 <br><br>
-                            <span style="font-weight: bolder;">Facebook</span> | https://www.facebook.com/angeladrian.zaragozarodriguez <br><br>
+                            <span style="font-weight: bolder;">Facebook</span> |
+                            https://www.facebook.com/angeladrian.zaragozarodriguez <br><br>
                         </p>
                     </div>
                 </div>
@@ -85,51 +86,80 @@
                     <input type="text" name="" id="" placeholder="Ingresa tu nombre" maxlength="100" required><br>
                     <input type="email" name="" id="" placeholder="Ingresa tu email" maxlength="150" required><br>
                     <input type="tel" name="" id="" placeholder="Ingresa tu telefono" maxlength="20" required><br>
-                    <textarea name="" id="" cols="30" rows="10" placeholder="¿Cual es la razon por la que quieres adoptar?" required></textarea><br>
-                    
-                    <label for="casa">Tipo de casa</label>
-                    <select name="" id="casa" required>
-                        <option value="">Departamento</option>
-                        <option value="">Unifamiliar</option>
-                        <option value="">Plurifamiliar</option>
-                        <option value="">Estudio</option>
-                        <option value="">Duplex</option>
-                    </select><br>
-                    
-                    <label for="patio">¿Cuentas con patio?</label>
-                    <input type="radio" name="patio" value="si" id="si"><label for="hombre">Si</label>
-                    <input type="radio" name="patio" value="no" id="no"><label for="mujer">No</label><br>
+                    <textarea name="" id="" cols="30" rows="5"
+                        placeholder="¿Cual es la razon por la que quieres adoptar?" required></textarea><br>
 
-                    <input type="number" name="" id="" placeholder="¿Cuantas personas viven en tu casa" required><br>
-                    
-                    <label for="animales">¿Tienes otro animales en tu casa?</label>
-                    <input type="radio" name="animales" value="si" id="si"><label for="hombre">Si</label>
-                    <input type="radio" name="animales" value="no" id="no"><label for="mujer">No</label>
-                    <input type="text" name="" id="" placeholder="¿Cuales?"><br>
+                    <div class="casa">
+                        <label for="casa">Tipo de casa:</label>
+                        <select name="" id="casa" required>
+                            <option value="">Departamento</option>
+                            <option value="">Unifamiliar</option>
+                            <option value="">Plurifamiliar</option>
+                            <option value="">Estudio</option>
+                            <option value="">Duplex</option>
+                        </select>
+                    </div>
 
-                    <input type="text" name="" id="" placeholder="¿Donde dormira tu nueva mascota?"><br>
+                    <div class="patio">
+                        <label for="patio">¿Cuentas con patio?</label>
+                        <div class="opciones">
+                            <input type="radio" name="patio" value="si" id="si">
+                            <label for="si">Si</label>
+                            <input type="radio" name="patio" value="no" id="no" checked>
+                            <label for="no">No</label>
+                        </div>
+                    </div>
 
-                    <input type="text" name="" id="" placeholder="¿Cuanto tiempo pasara solo la nueva mascota?"><br>
+                    <input type="number" name="" id="" placeholder="¿Cuantas personas viven en tu casa" required>
 
-                    <label for="niños">¿Tienes niños?</label>
-                    <input type="radio" name="niños" value="si" id="si"><label for="hombre">Si</label>
-                    <input type="radio" name="niños" value="no" id="no"><label for="mujer">No</label>
-                    <input type="number" name="" id="" placeholder="Cuantos?"><br>
+                    <div class="animales">
+                        <label for="animales">¿Tienes otros animales en tu casa?</label>
+                        <div class="opciones">
+                            <input type="radio" name="animales" value="si" id="si-ani">
+                            <label for="si-ani">Si</label>
+                            <input type="radio" name="animales" value="no" id="no-ani" checked>
+                            <label for="no-ani">No</label>
+                        </div>
+                        <input type="text" name="" id="" placeholder="¿Cuales?">
+                    </div>
 
-                    <label for="renta">¿Tienes casa propia?</label>
-                    <select name="" id="renta" required>
-                        <option value="">Rentada</option>
-                        <option value="">Casa Propia</option>
-                    </select><br>
+                    <input type="text" name="" id="" placeholder="¿Donde dormira tu nueva mascota?">
 
-                    <label for="foto">Sube una foto de un documento que te valide como persona</label>
-                    <input type="file" name="" id="foto"><br>
+                    <input type="text" name="" id="" placeholder="¿Cuanto tiempo pasara solo la nueva mascota?">
 
-                    <label for="nacimiento">Fecha de nacimiento</label>
-                    <input type="date" name="" id="nacimiento"><br>
+                    <div class="ninos">
+                        <label for="niños">¿Tienes niños?</label>
+                        <div class="opciones">
+                            <input type="radio" name="niños" value="si" id="si-ni">
+                            <label for="si-ni">Si</label>
+                            <input type="radio" name="niños" value="no" id="no-ni" checked>
+                            <label for="no-ni">No</label>
+                        </div>
+                        <input type="number" name="" id="" placeholder="¿Cuantos?">
+                    </div>
 
-                    <label for="terminos">Acepto terminos y condiciones</label>
-                    <input type="checkbox" name="terminos" value="terminos" id="terminos"><br>
+                    <div class="renta">
+                        <label for="renta">¿Tienes casa propia?</label>
+                        <select name="" id="renta" required>
+                            <option value="">Rentada</option>
+                            <option value="">Casa Propia</option>
+                        </select>
+                    </div>
+
+                    <div class="foto">
+                        <label for="foto">Sube una foto de un documento que te valide como persona:</label>
+                        <input type="file" name="" id="foto" required>
+                    </div>
+
+                    <div class="nacimiento">
+                        <label for="nacimiento">Fecha de nacimiento</label>
+                        <input type="date" name="" id="nacimiento" required>
+                    </div>
+
+                    <div class="terminos">
+                        <input type="checkbox" name="terminos" value="terminos" id="terminos">
+                        <label for="terminos">Acepto terminos y condiciones</label>
+                    </div>
 
                     <input type="submit" value="Enviar Formulario">
                 </form>
